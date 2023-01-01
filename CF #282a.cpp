0a80590372780c96655b043d8f771e8db98b6a8d
Mohammad Amaan Sharif <97755687+amaan-272@users.noreply.github.com>
2023-01-01 23:53:22 +0530

    CF #282A.cpp

--- CF #282a.cpp
+++ CF #282a.cpp
@@ -1,3 +1,15 @@
+// LeetCode Solution Set 
+// Mohammad Amaan Sharif
+// amaan_272
+
+// Problem: #282a: Bit++
+// Tags: Implementation/ Constructive
+
+// Problem Statement: Given an integer num, return the number of digits in num that divide num. An integer val divides nums if nums % val == 0.
+// Constraints: 
+// 1 <= num <= 109
+// num does not contain 0 as one of its digits.
+
 #include <bits/stdc++.h>
 using namespace std;
 
